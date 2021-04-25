1cd989589eab338c9741e94041ab57490210109a
Samantha Hicks <sjhicks@umich.edu>
2021-04-25 13:55:49 -0400

    final

--- from bs4 import BeautifulSoup.py
+++ from bs4 import BeautifulSoup.py
@@ -8,6 +8,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import matplotlib
+import matplotlib.pyplot as plt
 
 
 
@@ -75,68 +77,17 @@ def first_artist():
     for item in sorted_streams: 
         if item[0]== number_one:
             total_streams += item[1]
-        #final_tup_list.append(item, total_streams)
-    
-    #final_tup_list = [(number_one, total_streams)]
-    #print(final_tup_list)
     
     return final_result
 
-def second_artist():
-    dic = {}
-    lst = []
-    streams_dic = {}
-    table = soup.find('table', class_ = 'chart-table')
-    for artist_data in table.find_all('tr')[1:]:
-        track_name = artist_data.find('td', class_ = 'chart-table-track').contents[1].contents[0]
-        artist = artist_data.find('td', class_ = 'chart-table-track').contents[3].text[3:]
-        streams = artist_data.find('td', class_ = 'chart-table-streams').text
-        streams = streams.replace(",", "")
-        dic[artist] = dic.get(artist, 0) +1
-        streams_dic[artist] = streams_dic.get(artist, int(streams)) + int(streams)
-            #lst.append((track_name, streams))
-    sorted_dic = sorted(dic.items(), key = lambda x:x[1], reverse = True)
-    number_one = sorted_dic[0][0]
-        #print(sorted_dic)
-    sorted_streams = sorted(streams_dic.items(), key = lambda x:x[1], reverse = True)
-        #print(sorted_streams)
-        #print (sorted_streams[0])
-        #final_tup_list = []
-    total_streams = 0
-    for item in sorted_streams: 
-        if item[0]== number_one:
-            total_streams += item[1]
-        #final_tup_list.append(item, total_streams)
+def get_averages(lst):
+    average_streams = []
+    for artist in lst:
+        average = artist[1]/ artist[2]
+        average_streams.append((artist[0],artist[1],artist[2],average))
+    sorted_average = sorted(average_streams, key = lambda x:x[3], reverse = True)
+    return(sorted_average)
     
-    #final_tup_list = [(number_one, total_streams)]
-    #print(final_tup_list)
-    
-    return sorted_dic
-    #print(lstt)
-    #return final_tup_list
-
-    #for artist in final_tup_list:
-        #print(artist[0][0])
-   # print(streams_dic)
-   # print(number_one)
-    #return (total_streams)
-
-
-    #return sorted_dic[0][0]
-
-
-#print(sorted_dic[:10])
-#print(sorted_streams)
-number_one_artist = sorted_streams[0][0]
-number_two_artist = sorted_streams[1][0]
-number_three_artist = sorted_streams[2][0]
-number_four_artist = sorted_streams[3][0]
-number_five_artist = sorted_streams[4][0]
-number_six_artist = sorted_streams[5][0]
-number_seven_artist = sorted_streams[6][0]
-number_eight_artist = sorted_streams[7][0]
-number_nine_artist = sorted_streams[8][0]
-number_ten_artist = sorted_streams[9][0]
 
 def setUpDatabase(db_name):
      path = os.path.dirname(os.path.abspath(__file__))
@@ -144,26 +95,49 @@ def setUpDatabase(db_name):
      cur = conn.cursor()
      return cur, conn
 
-def setUpSongTable(first, cur,conn):
+def setUpSongTable(first, average, cur,conn):
     cur.execute('DROP TABLE IF EXISTS streams')
-    cur.execute('CREATE TABLE IF NOT EXISTS streams(Artist TEXT PRIMARY KEY, Streams INTEGER, Occurance INTEGER)')
-    for artist in first:
-        cur.execute("INSERT INTO streams (Artist, Streams, Occurance) VALUES (?,?,?)", (artist[0], artist[1],artist[2]))
+    cur.execute('CREATE TABLE IF NOT EXISTS streams(Artist TEXT PRIMARY KEY, Streams INTEGER, Occurance INTEGER, Average REAL)')
+    for artist in average:
+        cur.execute("INSERT INTO streams (Artist, Streams, Occurance, Average) VALUES (?,?,?,?)", (artist[0], artist[1],artist[2],artist[3]))
     conn.commit()
+    # for i in average:
+    #     cur.execute("INSERT INTO streams (Occurance, Average) VALUE (?)", (i[1]))
+    # conn.commit()
 
     # for i in range(len(sorted_streams)):
     #     cur.execute("INSERT INTO Categories (artist),streams) VALUES (?,?)",(sorted_streams[i][0],sorted_streams[i][1]))
     # conn.commit()
+
+def barchart_averages():
+    cur.execute("SELECT * FROM streams")
+    label_name = []
+    average_streams = []
+    for row in average:
+        label_name.append(row[0])
+    for row2 in average:
+        average_streams.append(row2[3])
+        
+    #print("LABEL NAMEMEMMEMEMEMMEMEE")
+    #print(label_name)
+
+
+    labels = [label_name[0], label_name[1],label_name[2],label_name[3], label_name[4],label_name[5], label_name[6],label_name[7],label_name[8], label_name[9]]
+    avg_streams = [average_streams[0],average_streams[1], average_streams[2],average_streams[3],average_streams[4], average_streams[5], average_streams[6],average_streams[7],average_streams[8], average_streams[9]]
+    plt.bar(labels, avg_streams, align = "center", color = ["red", "pink", "lightpink", "green", "deeppink", "navy", "lightblue", "lightgreen","purple","pink"])
+    plt.title("Comparison of Average Streams for Artists on the Top 200 Charts")
+    plt.ylabel("Average Song Streams (by the millions)")
+    plt.xlabel("Artist Name")
+    plt.savefig("artist_streams.png")
+    plt.show()
     
+    return ((label_name[0], average_streams[0]),(label_name[1], average_streams[1]), (label_name[2], average_streams[2]), (label_name[3], average_streams[3]), (label_name[4], average_streams[4]), (label_name[5], average_streams[5]), (label_name[6], average_streams[6]),(label_name[7], average_streams[7]), (label_name[8], average_streams[8]), (label_name[9], average_streams[9]))
+
+
 if __name__ == "__main__":
     first  = first_artist()
-    #second = second_artist()
-    # third = third_artist()
-    # fourth = fourth_artist()
-    # fifth = fifth_artist()
-    #print(first, second, third, fourth, fifth)
+    average = get_averages(first)
     cur, conn = setUpDatabase('streams.db')
-    setUpSongTable(first, cur,conn)
-    #conn = sqlite3.connect('streams.db')
-    #cur = conn.cursor()
-    #setUpSongTable(cur,conn)
+    setUpSongTable(first, average,cur,conn)
+    barchart_averages()
+    
